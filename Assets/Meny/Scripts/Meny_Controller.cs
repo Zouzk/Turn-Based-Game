827759a656b24f3aea79cf41be206bfb0bc8520a
LEARNET\jakob.lundberg1 <jakoblundbergsu15@gmail.com>
2017-02-27 16:25:07 +0100

    Fixade Menyn

--- Assets/Meny/Scripts/Meny_Controller.cs
+++ Assets/Meny/Scripts/Meny_Controller.cs
@@ -10,51 +10,75 @@ public class Meny_Controller : MonoBehaviour
 
 	private int listNr = 0;
 
+	enum Menu {MainMenu, ActionMenu, CombineMenu, Confirm};
+	Menu menu = Menu.MainMenu;
+
 	private List <Vector2> mainMeny;
 	private List <Vector2> actionMeny;
 	private List <Vector2> combineMeny;
 	private List <Vector2> currentMeny;
+	private List <Vector2> confirm;
+	//public Vector2 markerOffset;
+	private bool isOnAction;
 
+	public GameObject action;
+	public GameObject combine;
 
 	// Use this for initialization
 	void Start () 
 	{
 		mainMeny = new List <Vector2> ();
-		mainMeny.Add (new Vector2 (-263.0f, -96.0f));
-		mainMeny.Add (new Vector2 (-263.0f, -126.0f));
+		mainMeny.Add (new Vector2 (-260, 80));
+		mainMeny.Add (new Vector2 (-260, 35));
 
 		actionMeny = new List<Vector2> ();
-		actionMeny.Add (new Vector2 (, ));
-		actionMeny.Add (new Vector2 (, ));
-		actionMeny.Add (new Vector2 (, ));
-		actionMeny.Add (new Vector2 (, ));
-		actionMeny.Add (new Vector2 (, ));
-		actionMeny.Add (new Vector2 (, ));
+		//Water
+		actionMeny.Add (new Vector2 (-167, 82));
+		//Fire
+		actionMeny.Add (new Vector2 (-175, 54));
+		//Earth
+		actionMeny.Add (new Vector2 (-167, 21));
+		//Ice
+		actionMeny.Add (new Vector2 (-55, 82));
+		//Lightning
+		actionMeny.Add (new Vector2 (-17, 54));
+		//Block
+		actionMeny.Add (new Vector2 (-43, 21));
 
 		combineMeny = new List<Vector2> ();
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
-		combineMeny.Add (new Vector2 (, ));
+		//Water
+		combineMeny.Add (new Vector2 (362, 82));
+		//Fire
+		combineMeny.Add (new Vector2 (357, 54));
+		//Earth
+		combineMeny.Add (new Vector2 (364, 21));
+		//Ice
+		combineMeny.Add (new Vector2 (218, 82));
+		//Lightning
+		combineMeny.Add (new Vector2 (253, 52));
 
-		currentMeny = new List <Vector2>();
+		confirm = new List<Vector2> ();
+		//Confirm
+		confirm.Add (new Vector2 (100, 52.5f));
 
-		for (int i = 0; i < 
 
-		marker.transform.localPosition = mainMeny [listNr];
+		currentMeny = new List <Vector2>(mainMeny);
+
+		isOnAction = true;
+
+		transform.GetComponent<RectTransform>().anchoredPosition = currentMeny [listNr];
+
+		// Action.SetActive (true); Sets Gameobject to whenever it should be active or not
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+
+
+
+
+
 		if (Input.GetKeyDown (KeyCode.W))
 		{
 			listNr--;
@@ -64,6 +88,15 @@ public class Meny_Controller : MonoBehaviour
 				listNr = currentMeny.Count - 1;
 			}
 
+			if (menu == Menu.MainMenu && listNr == 0)
+			{
+				isOnAction = true;
+			} 
+
+			else if (menu == Menu.MainMenu && listNr != 0)
+			{
+				isOnAction = false;
+			}
 			MarkerPos ();
 		}
 
@@ -76,14 +109,197 @@ public class Meny_Controller : MonoBehaviour
 				listNr = 0;
 			}
 
+			if (menu == Menu.MainMenu && listNr == 0)
+			{
+				isOnAction = true;
+			} 
+
+			else if (menu == Menu.MainMenu && listNr != 0)
+			{
+				isOnAction = false;
+			}
+
 			MarkerPos ();
 		}
 
+		if (Input.GetKeyDown (KeyCode.A) && menu != Menu.MainMenu)
+		{
+			if (menu == Menu.ActionMenu && listNr >= 3)
+			{
+				listNr = listNr - 3;
+			} 
+
+			else if (menu == Menu.CombineMenu && listNr < 2)
+			{
+				listNr = listNr + 3;
+			}
+
+			if (listNr >= currentMeny.Count)
+			{
+				listNr = currentMeny.Count;
+			}
+
+			MarkerPos ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.D) && menu != Menu.MainMenu)
+		{
+			if (menu == Menu.ActionMenu && listNr < 3)
+			{
+				listNr = listNr + 3;
+			} 
+
+			else if (menu == Menu.CombineMenu && listNr >= 3)
+			{
+				listNr = listNr - 3;
+			}
+			if (listNr >= currentMeny.Count)
+			{
+				listNr = currentMeny.Count;
+			}
+
+			MarkerPos ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			
+			MenySwitcher (true);
+		
+		}
+
+		if (Input.GetKeyDown (KeyCode.B))
+		{
+			MenySwitcher (false);
+		}
+
+		if (isOnAction == true)
+		{
+			action.SetActive (true);
+			combine.SetActive (false);
+		}
+
+		else
+		{
+			action.SetActive (false);
+			combine.SetActive (true);
+		}
 	}
 
 
 	void MarkerPos ()
 	{
-		marker.transform.localPosition = currentMeny [listNr];
+		transform.GetComponent<RectTransform>().anchoredPosition = currentMeny [listNr]; //+ markerOffset;
+	}
+
+	void MenySwitcher (bool nextMenu) 
+	{
+		if (isOnAction == true)
+		{
+			if (menu == Menu.MainMenu)
+			{
+				MenyChanger (actionMeny, Menu.ActionMenu);
+			}
+
+			else if (menu == Menu.ActionMenu)
+			{
+				if (nextMenu == true)
+				{
+					MenyChanger (confirm, Menu.Confirm);
+				} 
+
+				else
+				{
+					MenyChanger (mainMeny, Menu.MainMenu);
+				}
+			}
+
+			else if (menu == Menu.Confirm)
+			{
+				if (nextMenu == true)
+				{
+					EndTurn ();
+				} 
+
+				else
+				{
+					MenyChanger (actionMeny, Menu.ActionMenu);
+				}
+			
+			}
+		
+		} 
+
+		else
+		{
+			if (menu == Menu.MainMenu)
+			{
+				MenyChanger (actionMeny, Menu.ActionMenu);
+			}
+
+			else if (menu == Menu.ActionMenu)
+			{
+				if (nextMenu == true)
+				{
+					MenyChanger (combineMeny, Menu.CombineMenu);
+				} 
+
+				else
+				{
+					MenyChanger (mainMeny, Menu.MainMenu);
+				}
+			}
+
+			else if (menu == Menu.CombineMenu)
+			{
+				if (nextMenu == true)
+				{
+					MenyChanger (confirm, Menu.Confirm);
+				} 
+
+				else
+				{
+					MenyChanger (actionMeny, Menu.ActionMenu);
+				}
+			} 
+
+			else if (menu == Menu.Confirm)
+			{
+				if (nextMenu == true)
+				{
+					EndTurn ();
+				} 
+
+				else
+				{
+					MenyChanger (combineMeny, Menu.CombineMenu);
+				}
+		
+			}
+		
+		}
+	
+	}
+
+	void MenyChanger (List<Vector2> menuList, Menu menuEnum)
+	{
+		currentMeny = new List <Vector2> (menuList);
+		menu = menuEnum;
+
+		if (menu == Menu.MainMenu && isOnAction == false)
+		{
+			listNr = 1;
+		} 
+
+		else
+		{
+			listNr = 0;
+		}
+		MarkerPos ();
+	}
+
+	public void EndTurn ()
+	{
+		
 	}
 }
